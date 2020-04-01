8aca3805af905148e17c9ebfde1c7ac6754ac145
Josiel da Silva Sousa <josielsilva84@gmail.com>
2020-03-31 21:33:21 -0300

    Commit 238

---
+++ SalesWebMVC/Models/SalerRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SalesWebMVC.Models.Enums;
+
+namespace SalesWebMVC.Models
+{
+    public class SalerRecord
+    {
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public double Amount { get; set; }
+        public SalerStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalerRecord()
+        {
+
+        }
+
+        public SalerRecord(int id, DateTime date, double amount, SalerStatus status, Seller seller)
+        {
+            Id = id;
+            this.date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public ICollection<SalerRecord> Sales { get; set; } = new List<SalerRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddSales(SalerRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalerRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial,DateTime final)
+        {
+            return Sales.Where(sr => sr.date >= initial && sr.date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
