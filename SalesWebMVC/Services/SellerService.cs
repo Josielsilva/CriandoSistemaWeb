cde0ad1662c81e1fb633461808546d77e57e4255
josiel da Silva <josielsilva@hotmail.com.br>
2020-04-06 18:55:19 -0300

    242

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _Context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _Context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _Context.Seller.ToList();
+        }
+    }
+}
